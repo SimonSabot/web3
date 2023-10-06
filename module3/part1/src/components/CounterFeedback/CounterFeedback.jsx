76828dde4c947501e0740d5869ae23688a9ced4d
RandomSkill <simon.sabot@Gmail.com>
2023-10-06 13:34:01 +0200

    module3-basic

--- module3/part1/src/components/CounterFeedback/CounterFeedback.jsx
+++ module3/part1/src/components/CounterFeedback/CounterFeedback.jsx
@@ -1,17 +1,31 @@
 import "./CounterFeedback.css" 
+import StatisticLine from "./StatisticLine";
 const CounterFeedback = ({good,neutral,bad})=>{
-
+    
     
         const sum =good+neutral+bad;
-        const pos = (good/sum)*100;
         
+        const pos = (good/sum)*100;
+        const avg = (good-bad)/sum;
+        
+
     return(
-    <div className="counterfeedback">
-   <p>good = {good}</p> 
-    <p>neutral = {neutral}</p>
-    <p> bad = {bad}</p>
-    <p> all = {sum}</p>
-    <p> positive = {pos} %</p>
-</div>)
+    <div>
+    <h2>Statistic</h2>
+    {sum===0 ? <p>no statistic for now</p> :
+    <table className="counterfeedback">
+    <tbody>
+   
+       <StatisticLine text="good" value={good}/> 
+        <StatisticLine text="neutral" value={neutral}/>
+        <StatisticLine text="bad" value={bad}/>
+        <StatisticLine text="sum" value={sum}/>
+      <StatisticLine text="average" value={avg}/> 
+      <StatisticLine text="Happy %" value={pos}/>
+    
+    </tbody>
+</table>
 }
+</div>
+)}
 export default CounterFeedback;
---
+++ module3/part1/src/components/CounterFeedback/StatisticLine.jsx
@@ -0,0 +1,11 @@
+const StatisticLine = ({text,value})=>{
+    
+
+    
+    return(
+    
+       <tr><td>{text} {value}</td></tr>
+       
+    )
+} 
+export default StatisticLine;
