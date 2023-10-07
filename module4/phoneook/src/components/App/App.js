95807e684478dfc67a80ae1485c48f46831aa5e5
Simon sabot <Simon.sabot@gmail.com>
2023-10-07 15:21:31 +0200

    bob

---
+++ module4/phoneook/src/components/App/App.js
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNewNameChange =(e)=>{
+    setNewName(e.target.value);
+  }
+  const addName =(e)=>{
+    e.preventDefault();
+   setPersons( persons.concat(newName));
+   setNewName('');
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input 
+          value={newName}
+          onChange={handleNewNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      ...
+    </div>
+  )
+}
+
+export default App
