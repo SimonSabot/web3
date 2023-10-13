4e6757544fac80e994519be142b51bd4fc8320d2
RandomSkill <simon.sabot@Gmail.com>
2023-10-13 15:27:47 +0200

    module4 -c fini

--- module4/phoneook/src/components/App/App.js
+++ module4/phoneook/src/components/App/App.js
@@ -1,28 +1,64 @@
-import { useState } from 'react'
+
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
-  const [newName, setNewName] = useState('')
+ 
+   
+
+  const [persons, setPersons] = useState([]);
+  const [newName, setNewName] = useState('');
+  const [newTel, setNewTel] = useState('');
   let i=0;
+
+  useEffect(() => {
+    axios
+      .get('http://localhost:3001/persons')
+      .then(response => {
+        setPersons(response.data)
+      })
+  }, [])
+
   const handleNewNameChange =(e)=>{
     setNewName(e.target.value);
   }
-  const addName =(e)=>{
-    e.preventDefault();
-   setPersons( persons.concat({name:newName}));
-   setNewName('');
+  const handleNewTelChange =(e)=>{
+    setNewTel(e.target.value);
   }
+
+
+  const addPerson =(e)=>{
+    e.preventDefault();
+    const bool =(persons.some(p=>(p.name===newName))&& newTel.match("^([0-9]{2}-[0-9]{2}-[0-9]{7})$"));
+    if(!bool){
+   setPersons( persons.concat({name:newName,tel:newTel}));
+   
+    }else{
+      alert(`${newName} is already present in the list`);
+    }
+   setNewName('');
+   setNewTel('');
+  }
+
+
+
 
   return (
     <div>
       <h2>Phonebook</h2>
-      <form onSubmit={addName}>
+      <form onSubmit={addPerson}>
         <div>
           name: <input 
           value={newName}
           onChange={handleNewNameChange}/>
+        </div>
+        <div>
+        number: <input 
+          value={newTel}
+          onChange={handleNewTelChange}
+          placeholder='00-00-0000000'
+          pattern='[0-9]{2}-[0-9]{2}-[0-9]{7}'
+          />
         </div>
         <div>
           <button type="submit">add</button>
@@ -30,7 +66,7 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       <ul>
-      {persons.map(p=>(<li key={i++}>{p.name}</li>))}
+      {persons.map(p=>(<li key={i++}>{p.name} {p.number}</li>))}
       </ul>
     </div>
   )
--- module4/phoneook/src/index.js
+++ module4/phoneook/src/index.js
@@ -3,11 +3,5 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App';
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  
-    <App />
-  
-);
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
